0117699eb1e8ea6a51ea7470a264fae10ee50f01
Mackenzie <mvlofthus@gmail.com>
2021-02-05 22:05:01 -0800

    got data to pull through from database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
 
   const [currentTime, setCurrentTime] = useState(1);
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('');
   
 
   useEffect(() => {
@@ -19,6 +19,25 @@ function App() {
       setErrorMessage(error.message);
     })
     }, []);
+
+    const [users, setUsers] = useState([]);
+    const [userCount, setUserCount] = useState(0);
+
+    useEffect(() => {
+      axios.get('/users')
+      .then((response) => {
+        console.log(response);
+        const userList = response.data.users;
+        setUsers(userList);
+        setUserCount(response.data.count);
+      })
+      .catch((error) => { 
+        setErrorMessage(error.message);
+      })
+    }, [])
+
+  
+
 
 
   return (
@@ -37,6 +56,12 @@ function App() {
           Learn React
         </a>
         <p> The current time is {currentTime}.</p>
+        <p> {userCount} Users: </p>
+        <ul>
+          {users.map((user) => {
+          return (<li key={user.id}>{user.name} - {user.email} </li>)
+        })}
+        </ul>
         <p> {errorMessage} </p>
       </header>
     </div>
